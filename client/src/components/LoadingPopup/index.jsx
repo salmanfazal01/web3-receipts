3e62735525e9420cbe189d1209cc8b2a3a6f7ce0
Salman Fazal <salmanfazal01@gmail.com>
2023-03-07 16:17:46 -0500

    Package.json cleanup

--- client/src/components/LoadingPopup/index.jsx
+++ client/src/components/LoadingPopup/index.jsx
@@ -23,6 +23,7 @@ const LoadingPopup = () => {
           p: 2,
           width: 420,
           bgcolor: "background.primary",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
         },
       }}
       slotProps={{
